53d59ceb93dae19b2636321b60a07a841aff1334
Dimitrije Jankov <dimitrijejankov@gmail.com>
2021-10-20 12:58:02 -0500

    Made two different partition schemes for the experiment.

--- applications/multiplication_chain/generate_multiplication_chain.cc
+++ applications/multiplication_chain/generate_multiplication_chain.cc
@@ -1,5 +1,6 @@
 
 #include "../../src/commands/compile_source_file.h"
+#include "../../src/commands/parsed_command.h"
 #include "../../src/commands/two_layer_compiler.h"
 #include "../../src/tensor/tensor.h"
 #include <cstdint>
@@ -15,26 +16,29 @@ const int32_t MUL_ID = 2;
 
 void generate_matrix(int32_t num_row, int32_t num_cols, int32_t row_split,
                      int32_t col_spilt,
-                     std::vector<abstract_command_t> &commands,
-                     std::map<std::tuple<int32_t, int32_t>, tid_t> &index) {
-
-  std::vector<command_param_t> param_data = {
-      command_param_t{.u = (std::uint32_t)(num_row / row_split)},
-      command_param_t{.u = (std::uint32_t)(num_cols / col_spilt)},
-      command_param_t{.f = 1.0f}, command_param_t{.f = 2.0f}};
-
+                     bbts::parsed_command_list_t &cmds,
+                     std::map<std::tuple<int32_t, int32_t>, tid_t> &index,
+                     int32_t node_start, int32_t node_end) {
+  
+  int32_t node_range = node_end - node_start;
+  node_id_t node = 0;
   for (auto row_id = 0; row_id < row_split; row_id++) {
     for (auto col_id = 0; col_id < col_spilt; col_id++) {
 
       index[{row_id, col_id}] = current_tid;
+      node = (node + 1) % node_range;
 
       // store the command
-      commands.push_back(
-          abstract_command_t{.ud_id = UNFORM_ID,
-                             .type = abstract_command_type_t::APPLY,
-                             .input_tids = {},
-                             .output_tids = {current_tid++},
-                             .params = param_data});
+      cmds.add_apply("uniform",
+                      {},
+                      {"dense"},
+                      false,
+                      {},
+                      {{current_tid++, node_start + node}},
+                      {command_param_t{.u = (std::uint32_t)(num_row / row_split)},
+                       command_param_t{.u = (std::uint32_t)(num_cols / row_split)},
+                       command_param_t{.f = 0.0f},
+                       command_param_t{.f = 1.0f}});
     }
   }
 }
@@ -55,11 +59,14 @@ void generate_mult(
         // store the c-idx
         c_tmp_index[{n_id, m_id}].push_back(current_tid);
 
+        auto a = a_index[{n_id, k_id}];
+        auto b = b_index[{k_id, m_id}];
+
         // store the command
         commands.push_back(abstract_command_t{
             .ud_id = MUL_ID,
             .type = abstract_command_type_t::APPLY,
-            .input_tids = {a_index[{n_id, k_id}], b_index[{k_id, m_id}]},
+            .input_tids = {a, b},
             .output_tids = {current_tid++},
             .params = param_data});
       }
@@ -106,10 +113,13 @@ void multiply(int32_t n_split, int32_t m_spilt, int32_t k_split,
 
 int main() {
 
+  int32_t num_nodes;
   int32_t n, m, k, l, p;
   int32_t n_split, m_split, k_split, l_split, p_split;
 
   // ([n, m] x [m, k]) * ([k, l] x [l, p])
+  std::cout << "the number of nodes : \n";
+  std::cin >> num_nodes;
   std::cout << "([n, m] x [m, k]) * ([k, l] x [l, p])\n";
   std::cout << "n :";
   std::cin >> n;
@@ -161,17 +171,28 @@ int main() {
   std::map<std::tuple<int32_t, int32_t>, tid_t> out_index;
 
   {
-    std::vector<abstract_command_t> commands;
-    generate_matrix(n, m, n_split, m_split, commands, a_index);
-    generate_matrix(m, k, m_split, k_split, commands, b_index);
-    generate_matrix(k, l, k_split, l_split, commands, c_index);
-    generate_matrix(l, p, l_split, p_split, commands, d_index);
+    bbts::parsed_command_list_t cmds;
+    generate_matrix(n, m, n_split, m_split, cmds, a_index, 0, num_nodes / 2);
+    generate_matrix(m, k, m_split, k_split, cmds, b_index, 0, num_nodes / 2);
+
+    generate_matrix(k, l, k_split, l_split, cmds, c_index, num_nodes / 2, num_nodes);
+    generate_matrix(l, p, l_split, p_split, cmds, d_index, num_nodes / 2, num_nodes);
 
     // write out the commands
-    std::ofstream gen("generate_matrix.sbbts");
-    compile_source_file_t gsf{.function_specs = funs, .commands = commands};
-    gsf.write_to_file(gen);
-    gen.close();
+    cmds.serialize("gen_partitioned.bbts");
+  }
+
+  {
+    bbts::parsed_command_list_t cmds;
+    current_tid = 0;
+    generate_matrix(n, m, n_split, m_split, cmds, a_index, 0, num_nodes);
+    generate_matrix(m, k, m_split, k_split, cmds, b_index, 0, num_nodes);
+
+    generate_matrix(k, l, k_split, l_split, cmds, c_index, 0, num_nodes);
+    generate_matrix(l, p, l_split, p_split, cmds, d_index, 0, num_nodes);
+
+    // write out the commands
+    cmds.serialize("gen_round_robin.bbts");
   }
 
   {
@@ -179,7 +200,8 @@ int main() {
     std::vector<abstract_command_t> commands;
     multiply(n_split, k_split, m_split, commands, a_index, b_index, ab_index);
     multiply(k_split, p_split, l_split, commands, c_index, d_index, cd_index);
-    multiply(n_split, p_split, k_split, commands, ab_index, cd_index, out_index);
+    multiply(n_split, p_split, k_split, commands, ab_index, cd_index,
+             out_index);
 
     // write out the commands
     std::ofstream gen("run.sbbts");
